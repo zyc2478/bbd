1411868199a24edc61eda1a1a20287e2eba95bbc
Richard Zeng <zyc2478@126.com>
2018-01-02 15:41:48 +0800

    删掉了多余的Employee类

--- src/test/java/com/autobid/util/Test1.java
+++ src/test/java/com/autobid/util/Test1.java
@@ -2,9 +2,7 @@ package com.autobid.util;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.Reader;
 import com.autobid.entity.User;
-import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
