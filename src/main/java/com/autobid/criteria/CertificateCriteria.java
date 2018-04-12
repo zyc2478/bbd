37d902c2fd44bb9719d9cfb05600a96bcfe69873
Richard Zeng <zyc2748@126.com>
2018-04-12 22:51:34 +0800

    去掉了VideoValidate的判定,并将原来CertificateCriteria的level由1-4改为1-3

--- src/main/java/com/autobid/criteria/CertificateCriteria.java
+++ src/main/java/com/autobid/criteria/CertificateCriteria.java
@@ -3,6 +3,7 @@ package com.autobid.criteria;
 import com.autobid.entity.Constants;
 import com.autobid.entity.Criteria;
 import com.autobid.util.ConfBean;
+import com.autobid.util.FormatUtil;
 import net.sf.json.JSONObject;
 
 @SuppressWarnings("deprecation")
@@ -13,20 +14,19 @@ public class CertificateCriteria implements Criteria, Constants {
     public void calc(JSONObject loanInfos, ConfBean cb) {
         int certificateValidate = Integer.parseInt(loanInfos.get("CertificateValidate").toString());
         int nciicIdentityCheck = Integer.parseInt(loanInfos.get("NciicIdentityCheck").toString());
-        int creditValidate = Integer.parseInt(loanInfos.get("CreditValidate").toString());
-        int videoValidate = Integer.parseInt(loanInfos.get("VideoValidate").toString());
-
-        certificateSum = certificateValidate + nciicIdentityCheck + creditValidate + videoValidate;
+        int creditValidate = Integer.parseInt(FormatUtil.nullToStr(loanInfos.get("CreditValidate").toString()));
+        //int videoValidate = Integer.parseInt(FormatUtil.nullToStr(loanInfos.get("VideoValidate").toString()));
+        certificateSum = certificateValidate + nciicIdentityCheck + creditValidate ;
     }
 
     public int getLevel(JSONObject loanInfos, ConfBean cb) {
         calc(loanInfos, cb);
         switch (certificateSum) {
-            case 2:
+            case 1:
                 return OK;
-            case 3:
+            case 2:
                 return GOOD;
-            case 4:
+            case 3:
                 return PERFECT;
             default:
                 return NONE;
